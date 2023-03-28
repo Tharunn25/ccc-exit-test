8b982d7dabf579d734afcc6f121a3daa05f37764
who Tf is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2023-03-28 15:28:38 +0530

    Update owner of the construction company.py

--- owner of the construction company.py
+++ owner of the construction company.py
@@ -1,30 +1,24 @@
-def maxProfit(costPerCut, salePrice, len):
-    # Find the shortest rod length
-    shortest_len = min(len)
-    
-    # Iterate over all possible sale lengths that are factors of the shortest length
+def maxProfit(costPerCut, salePrice, lengths):
     max_profit = 0
-    for i in range(1, shortest_len+1):
-        if shortest_len % i == 0:
-            # Calculate the number of rods that can be cut to the sale length
-            uniform_rods = 0
-            for j in len:
-                uniform_rods += j // i
-            
-            # Calculate the total length of all the rods that will be sold
-            total_length = uniform_rods * i
-            
-            # Calculate the number of cuts needed
-            total_cuts = 0
-            for j in len:
-                # Count the number of cuts needed for each rod
-                cuts = (j // i) - 1 if j % i == 0 else j // i
-                total_cuts += cuts
-            
-            # Calculate the profit for this sale length
-            profit = uniform_rods * i * salePrice - total_cuts * costPerCut
-            
-            # Update the maximum profit
-            max_profit = max(max_profit, profit)
-    
-    return max_profit
+
+    for sale_length in range(1, max(lengths) + 1):
+        sale_price_per_rod = salePrice * sale_length
+        profit = 0
+
+        for rod_length in lengths:
+            uniform_rods = rod_length // sale_length
+
+            if uniform_rods > 0:
+                extra_cut = 1 if rod_length % sale_length > 0 else 0
+                total_cuts = uniform_rods - 1 + extra_cut
+
+                costs = total_cuts * costPerCut
+                revenues = uniform_rods * sale_price_per_rod
+
+                if revenues > costs:
+                    profit += revenues - costs
+
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
